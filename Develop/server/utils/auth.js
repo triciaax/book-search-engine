68e779ebc4bd48e4329a8ce54798012117ea98d7
Tricia <tricianghiem@gmail.com>
2022-09-24 13:12:27 -0500

    updated middleware

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -10,7 +10,7 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
-  // function takes request as a parameter and checks to see if any of the parameters exist 
+  // function takes request as a parameter and checks to see if any of the parameters exist. if there is no token, it will return a request
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -22,6 +22,7 @@ module.exports = {
       return req;
     }
 
+    // trying to verify token and if its bad, it will print invalid token. if its good, it will update request
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
